23fcae32b33f5b411e132fe92940e2c0d4de7233
AbdullahNawaf <abdullahna360@gmail.com>
2023-09-02 19:38:40 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+# def my_name():
+#     print("My name is Abdullah Nawaf")
+# my_name()
+
+
+
+# def my_meal(food, drink):
+#     print(f"I like to eat {food} while drinking {drink}")
+
+# food= (input("insert food"))
+# drink= (input("insert drink"))
+# my_meal(food,drink)
+
+
+
+
+
+
+
+
+def cube(number):
+    return print(number**3)
+
+def by_three(number):
+    if number % 3 == 0 : 
+        cube(number)
+    else:
+        print("false")
+by_three(2)
+
+    
+    
